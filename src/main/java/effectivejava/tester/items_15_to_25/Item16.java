5fde27217751385973ec09dbaa9620e2554313e5
Park <oringnam@gmail.com>
2018-11-29 14:22:39 +0900

    item 16

---
+++ src/main/java/effectivejava/tester/items_15_to_25/Item16.java
@@ -0,0 +1,13 @@
+package effectivejava.tester.items_15_to_25;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item16 {
+    private String value1;
+    private int value2;
+}
