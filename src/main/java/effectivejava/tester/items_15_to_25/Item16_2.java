724303b6bfef50dc9493f3fecc86fd284bc0f39b
jinwon <oringnam@gmail.com>
2018-11-29 14:27:42 +0900

    item16

---
+++ src/main/java/effectivejava/tester/items_15_to_25/Item16_2.java
@@ -0,0 +1,10 @@
+package effectivejava.tester.items_15_to_25;
+
+/**
+ * package-private class는 가변인자를 public으로 제공해도 무방
+ * 단, 추상 개념만 올바르게 표현해주자
+ */
+class Item16_2 {
+    public String value1;
+    public int value2;
+}
